134868171f32dea7bf150f8b2bec2311c614dbfc
sambarth <samuel-barth@uiowa.edu>
2016-12-04 11:35:18 -0600

    adding jump/branch test circuits, made correction to cpu-bltzal.s

--- part-2/tests/cpu-bltzal.s
+++ part-2/tests/cpu-bltzal.s
@@ -1,7 +1,7 @@
 addi $s0, $zero, -1	#$s0: -1; $s1: 0; $s2: 0; $ra: 0
 addi $s1, $zero, 2	#$s0: -1; $s1: 2; $s2: 0; $ra: 0
 addi $s2, $zero, 0	#$s0: -1; $s1: 2; $s2: 0; $ra: 0
-bltzal finish		#NOOP
+bltzal $s0, finish	#NOOP
 add $s2, $zero, $zero	#$s0: 1; $s1: 2; $s2: 0; $ra: non-zero
 j end			#NOOP
 finish:
